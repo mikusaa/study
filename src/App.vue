a3478ccdc93733542655fd19231d614100589102
mikusaa <mikusaaaaa@gmail.com>
2021-05-22 21:40:43 +0800

    总之测试下

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Postcard 
+    v-for="post in list"
+     :key="post.count" 
+     :count="post.count" 
+     :data="post.data"
+    ></Postcard></div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Postcard from './components/postcard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Postcard
+  },
+  data(){
+    return{
+      list:[
+        {
+          count: 1000,
+          date: "20210501",
+        },
+         {
+          count: 2000,
+          date: "20210401",
+        },
+        {
+          count: 3000,
+          date: "20210301",
+        },
+        {
+          count: 4000,
+          date: "20210201",
+        },
+        {
+          count: 5000,
+          date: "20210101",
+        },
+        {
+          count: 6000,
+          date: "20200501",
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/postcard.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="post-card">
+
+<span v-if="count">{{ fomartDate(date) }} + {{ count }} 字</span>
+<div id="title"><b>制作原神个人信息名片</b></div>
+  <div id="subtitle">
+      使用「原神玩家信息卡片生成工具」，生成你的个人原神名片。
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+    date: {
+      type: String,
+      default: "20210101",
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    fomartDate(date) {
+      let year, month, day;
+      year = date.substring(0, 4);
+      month = date.substring(4, 6);
+      day = date.substring(6, 8);
+      let newDate = `${month} ${day}, ${year}`;
+      return newDate;
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+#post-card {
+  background-image: url(https://i.mikusa.cn/img-original/img/2021/01/05/02/26/58/86829113_p0.jpg);
+  background-size: cover;
+  width: 600px;
+  height: 150px;
+  margin: 16px;
+  float: left;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 16px;
+}
+#title {
+  font-size: 24px;
+  padding: 16px 0;
+}
+#subtitle {
+  font-size: 18px;
+  text-align: left;
+}
+</style>
